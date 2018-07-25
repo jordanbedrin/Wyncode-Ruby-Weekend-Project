168a7a49c6e1598973b9349f95584ad9058b30e5
Jordan Bedrin <jordanbedrin@aol.com>
2018-07-25 17:31:32 -0400

    fixed api error

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,7 +1,7 @@
 class GithubController < ApplicationController
   def index
     username = params[:username]
-    @profile = HTTParty.get("https://api.github.com/users/#{username}").parsed_response
+    @profile = HTTParty.get("https://api.github.com/users/harryy88").parsed_response
   end
 
 end
